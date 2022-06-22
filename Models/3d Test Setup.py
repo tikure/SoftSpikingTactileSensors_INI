a14f93de1f8fa750e2cc6d65b849932430309865
tikure <103319188+tikure@users.noreply.github.com>
2022-06-22 22:20:38 +0200

    Uploaded 350 Samples

--- Models/3d Test Setup.py
+++ Models/3d Test Setup.py
@@ -4,17 +4,15 @@ import time
 import numpy as np
 import sys
 import datetime
-
+#TODO change how files are named
 sys.path.append("./Functions")
 
 from SensorCollectionFunctions import *
+from Model_functions import *
 
-#s_sensor = serial.Serial(port = "COM8", baudrate=115200,bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
-s_printer = serial.Serial(port = "COM4", baudrate=250000) #COM4 on MSI
-#s_AFG = serial.Serial(port = "COM10", baudrate=115200,bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
-s_piezo = serial.Serial(port = "COM3", baudrate=9600) #COm3 on MSI
+s_Force = serial.Serial(port = "COM7", baudrate=115200,bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+s_sensor = serial.Serial(port="COM5", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+s_printer = serial.Serial(port="COM4", baudrate=250000)
 
-feedrate = "1600"
-while(1):
-    print(read_force(s_piezo))
-    time.sleep(0.1)
+setpos(10,10,-3, s_printer)
+print(read_force(s_Force))
